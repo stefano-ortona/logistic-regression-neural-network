1dc4e23bb53eca9baa953b6deb5d22bd2f8fa6fa
stefano-ortona <stefano.ortona@meltwater.com>
2020-04-07 12:36:53 +0100

    Application of logistic regression hot-dot/not hot-dog
    Implement the first use case of logistic regression algorithm with a hot-dog/not hot-dog app.
    The Kaggle dataset is used for training and testing the model https://www.kaggle.com/dansbecker/hot-dog-not-hot-dog/data.

---
+++ hot-dog/application.py
@@ -0,0 +1,47 @@
+from model.logistic_regression import model
+from model.logistic_regression import predict
+from dataset import load_dataset
+from dataset import read_one_image
+
+if __name__ == '__main__':
+    """
+    Build and train a model for the hot-dog/not hot-dog application, and try the model on new images.
+    
+    First download the dataset from Kaggle https://www.kaggle.com/dansbecker/hot-dog-not-hot-dog/data
+    and save it in your chosen BASE_FOLDER. Change the variable value 'base_folder' to your folder
+    
+    Then modify the 'new_image_path' variable to give a path for a new image to be predicted either as hot-dog
+    or not hot-dog
+    
+    Then run the application. The application will first build and train the Logistic Regression model with a Neural
+    Network approach. Once the model is built and trained, the model is used to predict whether a new chosen image is
+    actually a hot dog or not
+    
+    NOTE: training the model with standard parameters takes around ~40 seconds with 5000 iterations
+    """
+    # Define here your base folder containing training and test examples with hot dot/not hot dog images
+    # NOTE: it must be an absolute path
+    base_folder = "BASE_FOLDER/hot-dog-not-hot-dog/"
+
+    # Load the dataset into matrix of features
+    x_train, y_train, x_test, y_test = load_dataset(base_folder + "train/hot_dog", base_folder + "train/not_hot_dog",
+                                                    base_folder + "test/hot_dog", base_folder + "test/not_hot_dog")
+
+    # x_train, y_train, x_test, y_test = load_dataset_2()
+    # Train the model
+    # Feel free to modify parameters such as iteration, learning rate or threshold to check variations in performance
+    m = model(x_train, y_train, x_test, y_test,
+              iteration=5000, learning_rate=0.005, threshold=0.5, print_cost=True)
+
+    # Use the newly trained model to predict whether a new image is hot dog or not
+    # Define here path to the new image to be predicted
+    new_image_path = base_folder + ""
+    # Load the image as a feature vector
+    new_image = read_one_image(new_image_path)
+    # Predict the class on the new image
+    y_out = predict(new_image, m["w"], m["b"], 0.5)
+
+    assert y_out.shape == (1, 1)
+    out_class = "hot dog" if y_out[0][0] == 1 else "not hot-dog"
+
+    print("New image '{}' is predicted as '{}'".format(new_image_path, out_class))
---
+++ hot-dog/dataset.py
@@ -0,0 +1,71 @@
+import numpy as np
+from pathlib import Path
+from PIL import Image
+
+
+# All images in the dataset will be re-sized with the following pixel sizes
+pixel_size = (64, 64)
+
+
+def load_dataset(path_train_pos, path_train_neg, path_test_pos, path_test_neg):
+    """
+    Load from disk the training and test images examples.
+    Each image is converted into a (1, 256*256*3) vector, and values of the vector are normalized
+    with 256.
+
+    Keyword arguments:
+    path_train_pos -- A string path to the folder containing positive image training examples
+    path_train_neg -- A string path to the folder containing negative image training examples
+    path_train_pos -- A string path to the folder containing positive image test examples
+    path_train_neg -- A string path to the folder containing negative image test examples
+
+    Return
+    x_train -- matrix containing training examples, where each column of the matrix is one example
+    y_train -- a one line vector containing the label (0|1) for each train example
+    x_test  -- matrix containing test examples, where each column of the matrix is one example
+    y_test  -- a one line vector containing the label (0|1) for each test example
+    """
+    x_train_pos = read_images(path_train_pos)
+    print("Read a total of {} positive train examples".format(x_train_pos.shape[1]))
+    y_train_pos = np.ones((1, x_train_pos.shape[1]))
+    x_train_neg = read_images(path_train_neg)
+    print("Read a total of {} negative train examples".format(x_train_neg.shape[1]))
+    y_train_neg = np.zeros((1, x_train_neg.shape[1]))
+    y_train = np.concatenate((y_train_pos, y_train_neg), axis=1)
+    x_train = np.concatenate((x_train_pos, x_train_neg), axis=1)
+    x_test_pos = read_images(path_test_pos)
+    print("Read a total of {} positive test examples".format(x_test_pos.shape[1]))
+    y_test_pos = np.ones((1, x_test_pos.shape[1]))
+    x_test_neg = read_images(path_test_neg)
+    print("Read a total of {} negative test examples".format(x_test_neg.shape[1]))
+    y_test_neg = np.zeros((1, x_test_neg.shape[1]))
+    y_test = np.concatenate((y_test_pos, y_test_neg), axis=1)
+    x_test = np.concatenate((x_test_pos, x_test_neg), axis=1)
+    print("Read a total of {} train examples, {} test examples, with {} number of features".format(
+          x_train.shape[1], x_test.shape[1], x_train_pos.shape[0]))
+    return x_train, y_train, x_test, y_test
+
+
+def read_images(path):
+    path_list = list(Path(path).glob("*.jpg"))
+    m = len(list(path_list))
+    a_matrix = np.zeros((m, pixel_size[0] * pixel_size[1] * 3))
+    for i in range(m):
+        a_matrix[i] = read_one_image(path_list[i]).T
+    return a_matrix.T
+
+
+def read_one_image(path):
+    """
+    Load one image from disk and convert it to a single column vector.
+    The image is converted into a (1, 256*256*3) vector, and values of the vector are normalized
+    with 256.
+
+    Keyword arguments:
+    path -- A string path to the image file on Disk
+
+    Return
+    -- A (256 * 256 * 3, 1) vector representing the image features, with each element a real number in [0, 1]
+    """
+    image = Image.open(path).resize(pixel_size)
+    return (np.array(image) / 255.).reshape((1, pixel_size[0] * pixel_size[1] * 3)).T
